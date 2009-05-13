bc6eda010d05f67e07e8b8548a063b147039545a
Scott Taylor <scott@railsnewbie.com>
2009-05-13 17:36:23 -0400

    Add a spec for creating a column

--- spec/index_view/index_view_spec.rb
+++ spec/index_view/index_view_spec.rb
@@ -1,5 +1,14 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe IndexView do
+  before do
+    @view = Class.new(IndexView)
+  end
   
+  it "should be able to add a new column" do
+    @view.column :foo
+    column = @view.columns.first
+    
+    column.column_name.should equal(:foo)
+  end
 end
