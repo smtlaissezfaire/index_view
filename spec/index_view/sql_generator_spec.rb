a39d6ae2ba13eb1a314416255f939b11f957d9f0
Scott Taylor <scott@railsnewbie.com>
2009-07-27 17:27:00 -0400

    use better require statements in specs

--- spec/index_view/sql_generator_spec.rb
+++ spec/index_view/sql_generator_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe IndexView::SQLGenerator do
   it "should be a module" do
